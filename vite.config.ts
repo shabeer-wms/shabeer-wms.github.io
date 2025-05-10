fffab07048c59449fa054a8adad16285164e5b15
Muhammed Shabeer OP <androlite4@gmail.com>
2025-05-10 19:54:26 +0530

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: 'shabeer-wms.github.io',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/', // add trailing slash
+  plugins: [react()], // or vue() or svelte() depending on your project
+});
+
